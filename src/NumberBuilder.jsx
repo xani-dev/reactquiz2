c7d4795139770f00174ccb4fa4dfd4d4ac0e2797
xani-dev <xani.gn@gmail.com>
2023-05-30 10:27:56 -0400

    NumberBuilder Finished

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export const NumberBuilder = () => {
+	let [numString, setNumString] = useState('');
+	let [count, setCount] = useState(0);
 
-    return (
-        <>
-            <button>Add Number</button>
-            <button>Reset</button>
-            <br></br>
-            Numbers: 
-        </>
-    )
-}
+	const setBuilder = () => {
+		setCount(++count);
+		setNumString((numString += count + ' '));
+	};
+
+	return (
+		<>
+			<button
+				onClick={() => {
+					setBuilder();
+				}}
+			>
+				Add Number
+			</button>
+			<button
+				onClick={() => {
+					setNumString(' ');
+					setCount(0);
+				}}
+			>
+				Reset
+			</button>
+			<p>
+				Numbers: <span>{numString}</span>
+			</p>
+		</>
+	);
+};
