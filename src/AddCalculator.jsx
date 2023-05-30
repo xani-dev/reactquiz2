74efd013151e3fdcad351adbd5bd7dff2b580382
xani-dev <xani.gn@gmail.com>
2023-05-30 10:34:38 -0400

    AddCalculator Finished

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddCalculator = () => {
 
+    const [total, setTotal] = useState(0)
+
+    const additionHandler = (num) => {
+        setTotal(total+num)
+    }
+
     return (
-        <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
-            <br></br>
-            Total: 
-        </>
-    )
+			<>
+				<button
+					onClick={() => {
+						additionHandler(1);
+					}}
+				>
+					Add 1
+				</button>
+				<button
+					onClick={() => {
+						additionHandler(2);
+					}}
+				>
+					Add 2
+				</button>
+				<button
+					onClick={() => {
+						additionHandler(3);
+					}}
+				>
+					Add 3
+				</button>
+				<br></br>
+				<p>
+					Total: <span>{total}</span>
+				</p>
+			</>
+		);
 }
